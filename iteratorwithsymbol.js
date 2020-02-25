9f5c4d559fc09d81af1c51dd036b61eede9c6af4
ManavSharma03 <manav.appknit@gmail.com>
2020-02-25 15:08:08 +0530

    iterator

---
+++ iteratorwithsymbol.js
@@ -0,0 +1,20 @@
+
+let obj = {
+    name: ["a","b","c","d"],
+    age: 28,
+    skills: ["m", "n", "o"],
+    [Symbol.iterator](){
+        let i = 0;
+        return {
+            next:function(){
+            return obj.name.length > i ?
+            { done: false, value: obj.name[i++] }:
+            { done: true, value: undefined };
+            }
+        }
+    }
+}
+
+for(let j of obj){
+    console.log(j);
+}
